aa937cddd26b5e2f1403f614beeb5ed33d0346f6
shivanshu gupta <shivanshu.gupta4@gmail.com>
2019-09-25 16:25:47 +0530

    Add files via upload

---
+++ WebAPI/Models/ProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class ProfileModel
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string DOB { get; set; }
+        public string Language { get; set; }
+        public string GenToken { get; set; }
+    }
+}
